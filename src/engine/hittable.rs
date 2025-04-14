af2366667591da40c9837d6a9c79a6f95e09bde2
rayankermanshahani <rayan.k@gmx.com>
2025-04-13 20:26:53 -0400

    added function and logic to HitRecord structure to determine which side of a surface a ray is hitting

--- src/engine/hittable.rs
+++ src/engine/hittable.rs
@@ -1,15 +1,16 @@
-// src/hittable.rs
-
 use crate::engine::{
     ray::Ray,
-    vec3::{Point3, Vec3},
+    vec3::{self, Point3, Vec3},
 };
 
 pub struct HitRecord {
-    p: Point3,
-    normal: Vec3,
-    t: f64,
+    p: Point3,        // 3d point where ray intersects surface
+    normal: Vec3,     // surface normal at intersection point
+    t: f64,           // parametric distance along the ray where intersection occurs
+    front_face: bool, // flag indicating whether ray hit from outside (true) or inside (false)
 }
+
+impl HitRecord {}
 
 impl HitRecord {
     /// returns a HitRecord's point p
@@ -26,6 +27,10 @@ impl HitRecord {
     pub fn t(&self) -> f64 {
         self.t
     }
+    /// returns a HitRecord's front face
+    pub fn front_face(&self) -> bool {
+        self.front_face
+    }
 
     /// setter for HitRecord's point p
     pub fn set_p(&mut self, p: Point3) {
@@ -38,6 +43,25 @@ impl HitRecord {
     /// setter for HitRecord's t parameter
     pub fn set_t(&mut self, t: f64) {
         self.t = t;
+    }
+
+    /// setter for HitRecord's front face
+    pub fn set_front_face(&mut self, front_face: bool) {
+        self.front_face = front_face;
+    }
+
+    /// sets the hit record's normal vector
+    /// NOTE: outward_normal parameter is assumed to have unit length
+    pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: &Vec3) {
+        // determine if ray hit from outside (front face)
+        self.set_front_face(vec3::dot(&ray.direction(), &outward_normal) < 0.0);
+
+        // ensure surface normal always points against the ray
+        self.set_normal(if self.front_face() {
+            *outward_normal // ray is inside sphere
+        } else {
+            -(*outward_normal) // ray is outside sphere
+        });
     }
 }
 

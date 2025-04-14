da2487de6379b1b9bf4b9284d2317fd453c022cc
rayankermanshahani <rayan.k@gmx.com>
2025-04-13 21:25:06 -0400

    created a structure that stores a list of hittable objects that rays can intersect with

---
+++ src/engine/hittable_list.rs
@@ -0,0 +1,62 @@
+// src/engine/hittable_list.rs
+
+use crate::engine::{
+    hittable::{HitRecord, Hittable},
+    ray::Ray,
+    vec3::{Point3, Vec3},
+};
+
+use std::rc::Rc;
+
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    /// creates a new empty HittableList
+    pub fn new() -> HittableList {
+        HittableList {
+            objects: Vec::new(),
+        }
+    }
+
+    /// creates a new HittableList with a single object
+    pub fn with_object(object: Rc<dyn Hittable>) -> HittableList {
+        let mut list = HittableList::new();
+        list.add(object);
+        list
+    }
+
+    /// clears all objects from list
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    /// adds object to the list
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new();
+        let mut closest_so_far = ray_tmax;
+        let mut hit_anything = false;
+
+        for object in &self.objects {
+            if object.hit(ray, ray_tmin, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t();
+
+                // copy fields from temp_rec to rec
+                rec.set_p(temp_rec.p());
+                rec.set_normal(temp_rec.normal());
+                rec.set_t(temp_rec.t());
+                rec.set_front_face(temp_rec.front_face());
+            }
+        }
+
+        hit_anything
+    }
+}
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -3,6 +3,7 @@
 // declare submodules
 pub mod color;
 pub mod hittable;
+pub mod hittable_list;
 pub mod ray;
 pub mod sphere;
 pub mod vec3;

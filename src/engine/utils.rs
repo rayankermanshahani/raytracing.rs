166a74ad049ba62ca5993670811d4e902b245a48
rayankermanshahani <rayan.k@gmx.com>
2025-04-19 22:54:58 -0400

    added util functions for randomness

--- src/engine/utils.rs
+++ src/engine/utils.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use std::f64;
 
 // constants
@@ -9,4 +10,14 @@ pub const PI: f64 = f64::consts::PI;
 /// convert value from degrees to radians
 pub fn deg_to_rad(degrees: f64) -> f64 {
     degrees * PI / 180.0
+}
+
+/// returns a random real number in [0,1)
+pub fn random_0_to_1() -> f64 {
+    rand::rng().random()
+}
+
+/// returns a random real number in [0,1)
+pub fn random_min_to_max(min: f64, max: f64) -> f64 {
+    rand::rng().random_range(min..max)
 }
